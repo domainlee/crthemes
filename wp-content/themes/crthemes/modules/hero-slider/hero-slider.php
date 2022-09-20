e5ba800bf2f2af0b6f77d8ff08b4626e578c4986
admin <admin@Mien-Le-iMac.local>
2022-09-20 23:48:04 +0700

    u

--- wp-content/themes/crthemes/modules/hero-slider/hero-slider.php
+++ wp-content/themes/crthemes/modules/hero-slider/hero-slider.php
@@ -4,20 +4,16 @@
 
 <section class="hero">
     <div class="container-xl">
-        <div class="row py-6">
+        <div class="row py-6 align-items-center">
             <div class="col-5">
                 <h1 class="hero__heading">We work as an extension of your team</h1>
-            </div>
-            <div class="col-5 offset-2">
                 <p class="hero__intro">We are a team of quality theme developers to help you find creative, clean, quality themes. CRThemes makes it easier for you to develop, document easily.</p>
                 <div class="hero__button">
                     <a href="">Learn More <i class="icofont-arrow-down"></i></a>
                 </div>
             </div>
-        </div>
-        <div class="row pb-5">
-            <div class="col-12">
-                <figure class="ratio ratio-16x9 bg-cover bg-center bg-no-repeat lazy" data-src="<?= $image['url']; ?>"></figure>
+            <div class="col-6 offset-1">
+                <figure class="hero__image ratio ratio-1x1 bg-contain bg-center bg-no-repeat lazy" data-src="http://codexexpertbd.com/TF-ITEM/roktim/RoktimHtml/assets/img/h2-head.png"></figure>
             </div>
         </div>
     </div>
