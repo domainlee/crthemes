45ad59410a6bdf7a524aff868e6ebe9635361c28
Le Mien <lemien@Les-MacBook-Air.local>
2024-09-19 17:38:25 +0700

    Updated #01

--- wp-content/plugins/crt-manage-site/inc/class-crt-manage-site-register.php
+++ wp-content/plugins/crt-manage-site/inc/class-crt-manage-site-register.php
@@ -30,7 +30,14 @@ class CRT_Register
                 'callback' => array($this, 'active_site'),
                 'permission_callback' => '__return_true',
             ));
+
+            register_rest_route('transfer', 'domain', array(
+                'methods' => WP_REST_Server::EDITABLE,
+                'callback' => array($this, 'transfer'),
+                'permission_callback' => '__return_true',
+            ));
         });
+
     }
 
     public function active_site($request) {
@@ -64,7 +71,7 @@ class CRT_Register
         $db_exit = "exit;";
 
         $db_update_option = "UPDATE wp_options SET option_value = REPLACE(option_value, '$site_theme', '$site_client') WHERE option_name = 'home' OR option_name = 'siteurl';";
-        $db_update_option_code = "INSERT INTO `wp_options` (`option_id`, `option_name`, `option_value`, `autoload`) VALUES (NULL, 'crt_manage_code', '$code', 'yes');";
+        $db_update_option_code = "UPDATE wp_options SET option_value = '$code' WHERE wp_options.option_name = 'crt_manage_code';";
         $db_update_post_content = "UPDATE wp_posts SET post_content = REPLACE (post_content, '$site_theme', '$site_client');";
         $db_update_post_excerpt = "UPDATE wp_posts SET post_excerpt = REPLACE (post_excerpt, '$site_theme', '$site_client');";
         $db_update_post_value = "UPDATE wp_postmeta SET meta_value = REPLACE (meta_value, '$site_theme','$site_client');";
@@ -167,6 +174,14 @@ class CRT_Register
         echo '<p>Username: admin'. '</p>';
         echo '<p>Password: '.$wp_hasher. '</p>';
         return;
+    }
+
+    public function transfer($request) {
+        $data = $request->get_body();
+        $data_request = json_decode($data, true);
+
+        echo 'done';
+        exit();
     }
 
     public function action_after_submit ($cf7) {
